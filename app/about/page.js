a69b3849e97e0ac0e6627f78781f769e03a9087d
Hesham Saif <heshamsaif856@gmail.com>
2025-12-07 08:18:40 +0200

    nothing

--- app/about/page.js
+++ app/about/page.js
@@ -1,18 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Main from "../components/about/Main";
 
 export default function page() {
   return (
     <>
-      <div>
+      <div className="relative">
         <Navbar />
+        <div className="h-50"></div>
       </div>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-4xl font-bold mb-4">معلومات عنا</h1>
-        <p className="text-lg mb-8">هنا هنعمل معلومات عنا</p>
-      </div>
-      <div>
+      <div className="relative z-10">
+        <Main />
         <Footer />
       </div>
     </>
---
+++ app/components/contact/Main.js
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { useLanguage } from "../../context/LanguageContext";
+
+export default function main() {
+  const { t, language } = useLanguage();
+  return (
+    <div className="text-center h-100 text-6xl align-middle">
+      {/* Contact Page */}
+      <h2 className="pt-20">Contact Page</h2>
+    </div>
+  );
+}
